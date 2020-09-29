a06074e5e39ee668cec807ae3b37db2b3123aabc
Programmateur01 <git@juliencoutault.fr>
2020-09-29 13:37:04 +0200

    [SCRIPTS] fix redirection only if a target was specify

--- scripts/fixInternalLink.py
+++ scripts/fixInternalLink.py
@@ -11,21 +11,33 @@ def fixInternalLink(site, page, force=False):
     text = page.get()
     newText = text
     nbFix = 0
-    for link in re.findall(r"\[\[([\-\'\w\dÀ-ÿ\s\(\)]+)(\|([\-\'\w\dÀ-ÿ\s\(\)]+))?\]\]", text, flags=re.IGNORECASE):
-        pageLink = pywikibot.Page(site, link[0])
-        if not pageLink.isRedirectPage():
-            if link[2] and link[2] == link[0]:
-                nbFix += 1
-                newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}]]'.format(link[0]))
-        else:
-            while pageLink.isRedirectPage():
-                pageLink = pywikibot.Page(site, pageLink.getRedirectTarget().title())
-            if link[2] and pageLink.title() == link[2]:
-                nbFix += 1
-                newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}]]'.format(pageLink.title()))
+    for link in re.findall(r"\[\[([\-\'\w\dÀ-ÿ\s:\/\(\)]+)(\|([\-\'\w\dÀ-ÿ\s\(\)]+))?\]\]", text, flags=re.IGNORECASE):
+        link = list(link)
+        if link[0].startswith(' ') or link[0].endswith(' '):
+            # Remove useless space
+            nbFix += 1
+            newLink=  link[0].strip()
+            newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}{}]]'.format(newLink, link[1]))
+            link[0] = newLink
+
+        if link[2]:
+            # fix redirect only if target is specify
+            pageLink = pywikibot.Page(site, link[0])
+            if not pageLink.isRedirectPage():
+                if link[2].capitalize() == link[0].capitalize():
+                    # if the target is the same as the name
+                    nbFix += 1
+                    newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}]]'.format(link[2]))
             else:
-                nbFix += 1
-                newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}{}]]'.format(pageLink.title(), link[1]))
+                while pageLink.isRedirectPage():
+                    pageLink = pywikibot.Page(site, pageLink.getRedirectTarget().title())
+                if pageLink.title().capitalize() == link[2].capitalize():
+                    # if the target is the same as the name
+                    nbFix += 1
+                    newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}]]'.format(pageLink.title()))
+                else:
+                    nbFix += 1
+                    newText = newText.replace('[[{}{}]]'.format(link[0], link[1]), '[[{}{}]]'.format(pageLink.title(), link[1]))
 
     if nbFix:
         page.text = newText
@@ -42,11 +54,13 @@ def fixInternalLink(site, page, force=False):
             else:
                 page.save('Correction lien interne', minor=True, botflag=True)
 
+
 def fixInternalLinkStr(pageName, force=False):
     site = pywikibot.Site()
     site.login()
     page = pywikibot.Page(site, pageName)
     return fixInternalLink(site, page, force)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -58,6 +72,7 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+
 
 if __name__ == '__main__':
     args = parse_args()
